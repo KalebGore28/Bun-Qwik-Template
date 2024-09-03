dc30c780bc0a513ed1009371a4ae64ab9269116f
Kaleb Gore <91081805+KalebGore28@users.noreply.github.com>
2024-09-03 12:07:12 -0500

    push

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+/** @format */
+
+export default {
+	plugins: ['prettier-plugin-tailwindcss'],
+	semi: true,
+	trailingComma: 'all',
+	singleQuote: true,
+	printWidth: 80,
+	tabWidth: 4,
+	useTabs: true,
+	bracketSpacing: false,
+	jsxBracketSameLine: true,
+	insertPragma: true,
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+/** @format */
+
+import {component$} from '@builder.io/qwik';
+import type {DocumentHead} from '@builder.io/qwik-city';
 
 export default component$(() => {
 	return (
@@ -15,11 +17,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-	title: "Welcome to Qwik",
+	title: 'Welcome to Qwik',
 	meta: [
 		{
-			name: "description",
-			content: "Qwik site description",
+			name: 'description',
+			content: 'Qwik site description',
 		},
 	],
 };
