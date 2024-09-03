a445a3081aa5f3c4c21cdbbf023d173dc59ea41f
Kaleb Gore <91081805+KalebGore28@users.noreply.github.com>
2024-09-03 11:48:48 -0500

    push

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,15 @@
 export default {
-  plugins: ['prettier-plugin-tailwindcss'],
-}
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 4,
+  useTabs: true,
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  insertPragma: true,
+};
